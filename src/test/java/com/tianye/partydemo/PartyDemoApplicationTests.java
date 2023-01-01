a24759fdd4fea27303aa73232f771688f86250cb
ty2806 <ty2806.alpha@gmail.com>
2023-01-01 16:31:01 -0500

    Initial commit

---
+++ src/test/java/com/tianye/partydemo/PartyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tianye.partydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
